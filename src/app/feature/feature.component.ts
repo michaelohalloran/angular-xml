a059aba7a6d0603f55455dfb0262f074084496bb
Michael <kipsang20323@gmail.com>
2019-11-02 02:31:01 -0600

    fix map highlighting with input range

--- src/app/feature/feature.component.ts
+++ src/app/feature/feature.component.ts
@@ -3,13 +3,14 @@ import { parseString } from "xml2js";
 import { HttpClient } from "@angular/common/http";
 import Map from "ol/Map";
 import View from "ol/View";
-import { fromLonLat, transform } from "ol/proj";
+import { fromLonLat, toLonLat, transform } from "ol/proj";
 import { Tile as TileLayer, Vector as VectorLayer } from "ol/layer";
 import OSM from "ol/source/OSM";
 import { Vector as VectorSource } from "ol/source";
-import { Icon, Style, Fill, Stroke, Text } from "ol/style";
+import { Icon, Style, Text } from "ol/style";
 import Feature from "ol/Feature";
 import { Point } from "ol/geom";
+import { defaults as defaultControls } from "ol/control";
 
 @Component({
 	selector: "feature",
@@ -22,9 +23,11 @@ export class FeatureComponent implements OnInit {
 	units: any;
 	idsAndColors = [];
 	map: Map;
-	range: number;
+	range = 0;
 	initialSource: VectorSource;
 	private url = "assets/data/xmlData.xml";
+	EARTH_RADIUS = 6371e3;
+	METERS_TO_MILES = 1609.344;
 
 	constructor(private http: HttpClient, private renderer: Renderer2) {}
 
@@ -33,33 +36,16 @@ export class FeatureComponent implements OnInit {
 
 		this.getXmlData().subscribe(
 			(data) => {
-				// this.units = this.parseXmlData(data);
 				this.parseXmlData(data);
 				this.collectUnitIdAndColor(this.units);
 				this.displayUnitsOnMap();
 			},
-			(err) => console.log("err: ", err)
+			(err) => console.log("error in getting xml: ", err)
 		);
 	}
 
 	initializeMap() {
 		this.initialSource = new VectorSource();
-		const unitStyle = new Style({
-			fill: new Fill({ color: "blue" }),
-			stroke: new Stroke({ color: "green" }),
-			image: new Icon({
-				// anchor: [0.5, 46],
-				// color:
-				// imgSize: [ 100, 100 ],
-				// size: 50,
-				scale: 0.05,
-				src: "https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678111-map-marker-512.png",
-				opacity: 0.8
-			}),
-			text: new Text({
-				text: "Sample text"
-			})
-		});
 
 		const tileLayer = new TileLayer({
 			source: new OSM()
@@ -67,29 +53,86 @@ export class FeatureComponent implements OnInit {
 
 		const vectorLayer = new VectorLayer({
 			source: this.initialSource
-			// style: unitStyle
 		});
+		vectorLayer.set("id", "vectorLayer");
 
 		this.map = new Map({
 			layers: [ tileLayer, vectorLayer ],
+			controls: defaultControls({ attribution: false, rotate: false }),
 			target: this.renderer.selectRootElement("#map"),
 			view: new View({
 				center: fromLonLat([ -84, 32 ]),
-				zoom: 6
+				zoom: 7
 			})
 		});
 	}
 
 	// change this.range
 	adjustRange(evt: Event) {
-		// console.log("range evt: ", evt.value);
-		this.range = parseInt((evt.target as HTMLInputElement).value);
+		this.range = parseFloat((evt.target as HTMLInputElement).value);
 		console.log("range: ", this.range);
 	}
 
-	// apply highlight to all units within this.range miles (of what?)
+	// apply highlight to all units within this.range miles of map center
 	applyRange() {
 		// find map center, make radius extending this.range miles, apply highlights to all in that range
+		const mapCenter = toLonLat(this.map.getView().getCenter());
+		// retrieve the vectorLayer which contains the source which contains the unit features
+		const layer = this.map.getLayers().getArray().find((layer) => layer.get("id") === "vectorLayer");
+		// get marker features
+		const markers = layer.getSource().getFeatures();
+		markers.forEach((marker) => {
+			// get location of each marker
+			const location = marker.getGeometry().getCoordinates();
+			const lonLat = toLonLat(location);
+			// if latLon is within 10 miles of map.getViewport() latLon, apply yellow icon
+			const withinRange = this.calcDistanceFromMapCenter(mapCenter, lonLat) < this.range;
+			const markerId = marker.getStyle().getText().getText();
+			if (withinRange) {
+				marker.setStyle(this.getIconStyle([ 0, 255, 0, 1 ], markerId));
+			} else {
+				marker.setStyle(this.getIconStyle([ 128, 224, 255, 1 ], markerId));
+			}
+		});
+	}
+
+	// https://www.movable-type.co.uk/scripts/latlong.html
+	calcDistanceFromMapCenter(mapCenter: number[], latLon: number[]): number {
+		let [ mapLon, mapLat ] = mapCenter;
+		let [ markerLon, markerLat ] = latLon;
+		const radianCoords = [ mapLat, mapLon, markerLat, markerLon ].map((coord) => this.toRadians(coord));
+		const [ latMap, lonMap, latMarker, lonMarker ] = radianCoords;
+		const deltaLat = latMarker - latMap;
+		const deltaLon = lonMarker - lonMap;
+		const halfChordLength =
+			Math.sin(deltaLat / 2) ** 2 + Math.cos(latMap) * Math.cos(latMarker) * Math.sin(deltaLon / 2) ** 2;
+		const numerator = Math.sqrt(halfChordLength);
+		const denominator = Math.sqrt(1 - halfChordLength);
+		const angularDistance = 2 * Math.atan2(numerator, denominator);
+		return this.EARTH_RADIUS * angularDistance / 1000; // returns kilometers
+	}
+
+	toDegrees(radians: number): number {
+		return radians * 180 / Math.PI;
+	}
+
+	toRadians(degrees: number): number {
+		return degrees * Math.PI / 180;
+	}
+
+	getIconStyle(color: number[], id: string): Style {
+		return new Style({
+			image: new Icon({
+				anchor: [ 0.5, 1.2 ],
+				color,
+				scale: 0.05,
+				crossOrigin: "anonymous",
+				src: "https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678111-map-marker-512.png"
+			}),
+			text: new Text({
+				text: id
+			})
+		});
 	}
 
 	getXmlData() {
@@ -98,7 +141,6 @@ export class FeatureComponent implements OnInit {
 
 	collectUnitIdAndColor(units: any[]) {
 		this.idsAndColors = units.reduce((collection, nextUnit) => {
-			// const { ID, Gui: [ { Color: [ Blue, Green, Red ] } ] } = nextUnit;
 			const { ID, Location: [ { Lat, Lon } ], Gui: [ { Color: [ { Red, Green, Blue } ] } ] } = nextUnit;
 			collection.push({
 				id: ID.join(""),
@@ -107,50 +149,26 @@ export class FeatureComponent implements OnInit {
 			});
 			return collection;
 		}, []);
-		console.log("ids, colors, location: ", this.idsAndColors);
 	}
 
 	displayUnitsOnMap() {
 		if (this.idsAndColors) {
 			this.idsAndColors.forEach((unit) => {
-				// this.map.addFeature()
 				const { color, id, location: { lat, lon } } = unit;
 				const marker = new Feature({
 					geometry: new Point(transform([ lon, lat ], "EPSG:4326", "EPSG:3857"))
-					// name: "Sample"
 				});
-				// const markerStyle = new Style({
-				// 	fill: new Fill({ color: "green" })
-				// });
-				// marker.setStyle(markerStyle);
-				marker.setStyle(
-					new Style({
-						image: new Icon({
-							color,
-							scale: 0.05,
-							crossOrigin: "anonymous",
-							src: "https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678111-map-marker-512.png"
-						}),
-						text: new Text({
-							text: id
-						})
-					})
-				);
+
+				marker.setStyle(this.getIconStyle(color, id));
 				this.initialSource.addFeature(marker);
 			});
 		}
-		// add them as overlays? features? to the map
-		// use this.unit.color for each one's color
-		// use this.unit.id for adding innerHTML
-		// this.unit.location is an object w/ lat and lon props; use this for locations
 	}
 
 	parseXmlData(xml) {
 		return parseString(xml, (err, result) => {
 			if (err) console.log("parse err: ", err);
 			this.units = result.Interview.Unit;
-			console.log("units: ", this.units);
-			// return result;
 		});
 	}
 }
